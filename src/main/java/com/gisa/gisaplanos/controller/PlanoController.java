985cc83ceab457b99529fbd5236859fa037863cf
Andrei Filipini <andrei.filipini@sigma.com.br>
2022-01-28 20:03:30 -0300

    ajuste formatação

--- src/main/java/com/gisa/gisaplanos/controller/PlanoController.java
+++ src/main/java/com/gisa/gisaplanos/controller/PlanoController.java
@@ -1,36 +1,42 @@
 package com.gisa.gisaplanos.controller;
 
-import java.util.List;
-import java.util.stream.Collectors;
-
+import com.gisa.gisacore.exception.InfraException;
+import com.gisa.gisaplanos.dto.PlanoDTO;
+import com.gisa.gisaplanos.model.Plano;
+import com.gisa.gisaplanos.model.service.PlanoService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.gisa.gisaplanos.dto.PlanoDTO;
-import com.gisa.gisaplanos.model.Plano;
-import com.gisa.gisaplanos.model.service.PlanoService;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/plano")
 public class PlanoController {
 
-	@Autowired
-	private PlanoService planoService;
+    @Autowired
+    private PlanoService planoService;
 
-	@GetMapping
-	public ResponseEntity<List<PlanoDTO>> findAll() {
-		List<Plano> planos = planoService.findAll();
-		return ResponseEntity.ok(planos.stream().map(this::toDTO).collect(Collectors.toList()));
-	}
+    @GetMapping
+    public ResponseEntity<List<PlanoDTO>> findAll() {
+        try {
+            return ResponseEntity.ok(
+                    planoService.findAll().stream()
+                            .map(this::toDTO)
+                            .collect(Collectors.toList()));
+        } catch (InfraException e) {
+            return ResponseEntity.internalServerError().build();
+        }
+    }
 
-	private PlanoDTO toDTO(Plano plano) {
-		return PlanoDTO.builder().id(plano.getId()).descricao(plano.getDescricao()).ativo(plano.isAtivo())
-				.qtdConsultasAno(plano.getQtdConsultasAno()).qtdExamesAno(plano.getQtdExamesAno())
-				.valor(plano.getValor()).tipoAtendimento(plano.getTipoAtendimento()).tipoPlano(plano.getTipoPlano())
-				.build();
-	}
+    private PlanoDTO toDTO(Plano plano) {
+        return PlanoDTO.builder().id(plano.getId()).descricao(plano.getDescricao()).ativo(plano.isAtivo())
+                .qtdConsultasAno(plano.getQtdConsultasAno()).qtdExamesAno(plano.getQtdExamesAno())
+                .valor(plano.getValor()).tipoAtendimento(plano.getTipoAtendimento()).tipoPlano(plano.getTipoPlano())
+                .build();
+    }
 
 }
