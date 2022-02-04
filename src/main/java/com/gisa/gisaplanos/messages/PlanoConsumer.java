25cf30c282c8e8fe223208fa477d5a6a39393cb6
Andrei Filipini <andrei.filipini@sigma.com.br>
2022-02-04 07:51:18 -0300

    conectando mysql

--- src/main/java/com/gisa/gisaplanos/messages/PlanoConsumer.java
+++ src/main/java/com/gisa/gisaplanos/messages/PlanoConsumer.java
@@ -1,9 +1,12 @@
 package com.gisa.gisaplanos.messages;
 
+import com.gisa.gisacore.messages.AbstractRabbitConsumer;
 import com.gisa.gisaplanos.dto.AlteraPlanoRequestDTO;
 import com.gisa.gisaplanos.dto.AlteraPlanoResponseDTO;
 import com.gisa.gisaplanos.model.service.PlanoService;
 import com.google.gson.Gson;
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Value;
@@ -12,8 +15,9 @@ import org.springframework.stereotype.Component;
 
 import javax.inject.Inject;
 
+@Slf4j
 @Component
-public class PlanoConsumer {
+public class PlanoConsumer extends AbstractRabbitConsumer {
 
     @Inject
     private RabbitTemplate rabbitTemplate;
@@ -25,11 +29,21 @@ public class PlanoConsumer {
     private String changePlanResultQueueName;
 
     @RabbitListener(queues = {"${queue.changePlan}"})
-    private void receive(@Payload String body) {
+    protected void receive(@Payload String body) {
+        executeLoggin(body);
+    }
+
+    @Override
+    protected void execute(@Payload String body) {
         Gson gson = new Gson();
         AlteraPlanoRequestDTO request = gson.fromJson(body, AlteraPlanoRequestDTO.class);
 
         AlteraPlanoResponseDTO response = new AlteraPlanoResponseDTO(request.getIdTransacao(), planoService.isAtivo(request.getIdPlano()));
         rabbitTemplate.convertAndSend(this.changePlanResultQueueName, gson.toJson(response));
+    }
+
+    @Override
+    protected Logger getLogger() {
+        return log;
     }
 }
