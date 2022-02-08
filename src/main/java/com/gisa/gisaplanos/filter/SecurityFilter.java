c7707f764a8dac20fd260e5fd255a47951e53060
Andrei Filipini <andrei.filipini@sigma.com.br>
2022-02-07 21:34:58 -0300

    melhora jwtTokenUtil

--- src/main/java/com/gisa/gisaplanos/filter/SecurityFilter.java
+++ src/main/java/com/gisa/gisaplanos/filter/SecurityFilter.java
@@ -1,13 +1,11 @@
 package com.gisa.gisaplanos.filter;
 
 import com.gisa.gisacore.util.JwtTokenUtil;
-import com.gisa.gisacore.util.StringUtil;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.annotation.Order;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
 
-import javax.annotation.PostConstruct;
+import javax.inject.Inject;
 import javax.servlet.*;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -17,39 +15,19 @@ import java.io.IOException;
 @Order(1)
 public class SecurityFilter implements Filter {
 
-    @Value("${jwt.secret}")
-    private String secret;
-
-    @Value("${jwt.expiresIn}")
-    private Integer expiresIn;
-
+    @Inject
     private JwtTokenUtil jwtTokenUtil;
-
-    @PostConstruct
-    private void init() {
-        this.jwtTokenUtil = new JwtTokenUtil(secret, expiresIn);
-    }
 
     @Override
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
-        String jwtToken = getJwtToken((HttpServletRequest) request);
-        if (!this.jwtTokenUtil.isExpired(jwtToken) && this.jwtTokenUtil.verifyContainRole(getJwtToken((HttpServletRequest) request), "ASSOCIADO", "CONVENIADO")) {
+        String jwtToken = jwtTokenUtil.getJwtToken((HttpServletRequest) request);
+        if (!this.jwtTokenUtil.isExpired(jwtToken) && this.jwtTokenUtil.verifyContainRole(jwtToken, "ASSOCIADO", "CONVENIADO")) {
             chain.doFilter(request, response);
             response.getOutputStream().println();
         } else {
             HttpServletResponse httpResponse = (HttpServletResponse) response;
             httpResponse.setStatus(HttpStatus.UNAUTHORIZED.value());
         }
-    }
-
-    private String getJwtToken(HttpServletRequest request) {
-        HttpServletRequest req = request;
-        String token = req.getHeader("Authorization");
-
-        if(StringUtil.isNotBlank(token)) {
-            return token.replace("Bearer ",  "");
-        }
-        return null;
     }
 
 }
